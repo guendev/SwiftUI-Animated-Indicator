91718fdbaa521e7c813b5ea4aef806659618d715
yuan <dnstylish@gmail.com>
2021-10-19 20:29:10 +0700

    Initial Commit

---
+++ Shared/SwiftUI_Animated_IndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Animated_IndicatorApp.swift
+//  Shared
+//
+//  Created by Yuan on 19/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Animated_IndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

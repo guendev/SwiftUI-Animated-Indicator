0f098ee0239fc5b9b8533c563fca92027592f54b
yuan <dnstylish@gmail.com>
2021-10-21 10:59:25 +0700

    done

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        GeometryReader { proxy in
+            
+            Home(screenSize: proxy.size)
+                .preferredColorScheme(.dark)
+            
+        }
+        
     }
 }
 
